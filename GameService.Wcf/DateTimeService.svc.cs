1e1b1e3c57984df5fab0ea4691bab27a36f7fa22
valerymargunov <valery.margunov@itechart-group.com>
2015-07-11 20:24:58 +0300

    Created the DateTimeService service

---
+++ GameService.Wcf/DateTimeService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GameService.Wcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DateTimeService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select DateTimeService.svc or DateTimeService.svc.cs at the Solution Explorer and start debugging.
+    public class DateTimeService : IDateTimeService
+    {
+        public DateTime GetCurrentTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ GameService.Wcf/IDateTimeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace GameService.Wcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDateTimeService" in both code and config file together.
+    [ServiceContract]
+    public interface IDateTimeService
+    {
+        [WebInvoke(UriTemplate = "GetCurrentTime/", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml, Method = "GET")]
+        DateTime GetCurrentTime();
+    }
+}
